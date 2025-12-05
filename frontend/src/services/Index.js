2b9db5e7338ea451379b29c78c455d5db0f4d21b
Jose David <leonruizjosedavid44@gmail.com>
2025-12-04 22:31:48 -0500

    add backend

---
+++ frontend/src/services/Index.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "http://localhost:3000",
+});
+
+// Obtener todas las tareas
+export const getTasks = () => api.get("/tasks").then((r) => r.data);
+
+// Crear nueva tarea
+export const createTask = (task) => api.post("/tasks", task).then((r) => r.data);
+
+// Actualizar tarea (PUT para reemplazarla completa)
+export const updateTask = (id, updatedTask) =>
+  api.put(`/tasks/${id}`, updatedTask).then((r) => r.data);
+
+// Eliminar tarea
+export const deleteTask = (id) => api.delete(`/tasks/${id}`);
